3203fc1d3f0adbe3ced2609df3311ee82d5c8111
Xcode302 <96620646+Xcode302@users.noreply.github.com>
2022-01-06 10:11:38 +0530

    Update parity220.py

--- parity220.py
+++ parity220.py
@@ -91,27 +91,27 @@ def hero():
             n=int(current)%10
             if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
                 if current in numbers:
-                    print(newperiod+1," : 💥🔴RED1🔴💥")
+                    print(newperiod+1," : RED")
                 else:
-                    print(newperiod+1," : 💥🟢GREEN1🟢💥")
+                    print(newperiod+1," : GREEN")
             else:
                 if current in numbers:
-                    print(newperiod+1," :  💥🔴RED2🔴💥")
+                    print(newperiod+1," : GREEN")
                 else:
-                    print(newperiod+1," : 💥🔴RED3🔴💥")
+                    print(newperiod+1," : RED")
         if i in thatway:
             m=getSum(current)+1
             n=int(current)%10
             if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
                 if current in numbers:
-                    print(newperiod+1,": 💥💥🟢GREEN2🟢💥💥")
+                    print(newperiod+1,": RED")
                 else:
-                    print(newperiod+1,": 💥💥🟢GREEN3🟢💥💥")
+                    print(newperiod+1,": GREEN")
             else:
                 if current in numbers:
-                    print(newperiod+1,": 💥💥🔴RED4🔴💥💥")
+                    print(newperiod+1,": GREEN")
                 else:
-                    print(newperiod+1,": 💥💥🟢GREEN4🟢💥💥")
+                    print(newperiod+1,": RED")
         i=i+1
         newperiod+=1
         numbers.append(current)
